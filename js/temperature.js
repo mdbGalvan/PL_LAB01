2dc3078b995cbade3dfd79f5070bad19592c0695
mdbGalvan <magomenlopark@gmail.com>
2014-02-03 01:43:56 +0000

    Add JS
    Se añade el código en JavaScript que contiene las funciones que calcula
    el cambio de temperatura y rellena el recuedro con la información
    faltante.

---
+++ js/temperature.js
@@ -0,0 +1,47 @@
+function calculate() {
+  var result;
+  var original = document.getElementById("original");
+  var temp = original.value;
+  var regexp = /[-+]?\d*\.?\d+([eE][-+]?\d+)?[cCfF]/;
+  
+  var m = temp.match(regexp);
+  
+  if (m) {
+    var num = temp.match(/[-+]?\d*\.?\d+([eE][-+]?\d+)?/);
+    var type = temp.match(/[cCfF]/);
+    num = parseFloat(num);
+    if (type == 'c' || type == 'C') {
+      result = (num * 9/5)+32;
+      result += "F";
+    }
+    else {
+      result = (num - 32)*5/9;
+      result += "C";
+    }
+    converted.innerHTML = result;
+  }
+  else {
+    converted.innerHTML = "ERROR! Try something like '-4.2C' instead";
+  }
+}
+
+function stuff () {
+	
+	document.getElementById('sol1').style.color = '#0101ff';
+    //document.getElementById('sol1').style.fontWeight = 'bold';
+	document.getElementById('sol2').style.color = '#0101ff';
+	document.getElementById('sol3').style.color = '#0101ff';
+	document.getElementById('sol4').style.color = '#0101ff';
+	document.getElementById('sol5').style.color = '#0101ff';
+	document.getElementById('sol6').style.color = '#0101ff';
+	document.getElementById('sol7').style.color = '#0101ff';
+
+	document.getElementById("sol1").innerHTML = "original";
+	document.getElementById("sol2").innerHTML = "[-+]?\d*\.?\d+([eE][-+]?\d+)?[cCfF]";
+	document.getElementById("sol3").innerHTML = "regexp";
+	document.getElementById("sol4").innerHTML = "temp.match(/[-+]?\d*\.?\d+([eE][-+]?\d+)?/)";
+	document.getElementById("sol5").innerHTML = "temp.match(/[cCfF]/)";
+	document.getElementById("sol6").innerHTML = "F;";
+	document.getElementById("sol7").innerHTML = "C;";
+	
+}
